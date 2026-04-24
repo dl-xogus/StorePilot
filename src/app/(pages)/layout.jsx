0ad1188aa736cbcc4bd4e6f92502590069033764
woogangsomgang <jinyunseo0905@gmail.com>
2026-04-24 18:30:10 +0900

    1234

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,10 +1,27 @@
+"use client"
 import Sidebar from "@/components/layout/Sidebar";
 import layout from "@/app/(pages)/layout.module.css"
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
+    const [path,setPath] = useState()
+    const url = window.location.pathname;
+    let bln=false;
+
+    useEffect(function(){        
+        switch(url.substring(1)){
+            case 'signup' : bln=false; break;
+            case 'login' : bln=false; break;
+            case 'onboarding' : bln=false; break;
+            default:bln=true;
+        }
+        setPath(bln)
+    },[url])
+
+
     return (
         <div className={layout.layout}>
-            <Sidebar />
+            { path && <Sidebar /> }
             {children}
         </div>
     );
